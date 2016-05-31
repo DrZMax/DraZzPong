a7eea7919e92ece51757b2c951c52a21788963f1
MaximeST <drazz951@gmail.com>
2016-05-31 00:33:05 -0400

    Initial Commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public float speed = 15;
+    private Rigidbody2D rb;
+    private int startingPlayer = 1;
+    public string startKey = "space";
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKeyDown(startKey))
+        {
+            if(startingPlayer == 1)
+            {
+                rb.velocity = Vector2.right * speed;
+            }
+            else
+            {
+                rb.velocity = Vector2.left * speed;
+            }
+        }
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "rightPaddle" || col.gameObject.name == "leftPaddle")
+        {
+            paddleCollision(col);
+        }
+
+        verticalWallCollision(col);
+
+    }
+
+    float calculatePosition(Vector2 ballPosition, Vector2 paddlePosition, float paddleHeight)
+    {
+        float value = (ballPosition.y - paddlePosition.y) / paddleHeight;
+        return value;
+    }
+
+    void paddleCollision(Collision2D col)
+    {
+        float direction = 1;
+
+        if (col.gameObject.name == "rightPaddle")
+        {
+            direction = -1;
+        }
+
+        if (col.gameObject.name == "leftPaddle")
+        {
+            direction = 1;
+        }
+
+        float y = calculatePosition(transform.position, col.transform.position, col.collider.bounds.size.y);
+        Vector2 directionRebound = new Vector2(direction, y).normalized;
+        rb.velocity = directionRebound * speed;
+    }
+
+    void verticalWallCollision(Collision2D col)
+    {
+        if(col.gameObject.name == "leftWall")
+        {
+            rb.velocity = Vector2.zero;
+            startingPlayer = 1;
+            rb.MovePosition(Vector2.zero);
+        }
+
+        if (col.gameObject.name == "rightWall")
+        {
+            rb.velocity = Vector2.zero;
+            startingPlayer = 2;
+            rb.MovePosition(Vector2.zero);
+        }
+
+    }
+}
---
+++ Assets/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+    public float speed = 25;
+    public string axis = "Vertical";
+    private Rigidbody2D rBody;
+
+	// Use this for initialization
+	void Start () {
+        rBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float vertical = Input.GetAxisRaw(axis);
+        rBody.velocity = new Vector2(0, vertical) * speed;
+    }
+}
